6e654cc80b409ea04ef33ed15710a701f63da9e2
Pauloalves8039 <pj38alves@gmail.com>
2023-09-11 16:53:56 -0300

    Organizando a estrutura do projeto

---
+++ src/Products.Dapper.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Dapper.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
