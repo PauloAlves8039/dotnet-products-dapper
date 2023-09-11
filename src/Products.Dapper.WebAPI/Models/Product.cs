5c2e86dc06537f483cfab7ec965e4c54bd3fea91
Pauloalves8039 <pj38alves@gmail.com>
2023-09-11 16:59:33 -0300

    Criando classe de modelo para produto

---
+++ src/Products.Dapper.WebAPI/Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Products.Dapper.WebAPI.Utilities;
+
+namespace Products.Dapper.WebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(50)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(5)]
+        [MaxLength(100)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        [Required(ErrorMessage = "The Price is Required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DisplayName("Price")]
+        [DecimalRange(ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("quantity")]
+        [Required(ErrorMessage = "The Quantity is Required")]
+        [Range(1, 9999, ErrorMessage = "Quantity must be between 1 and 9999")]
+        [DisplayName("Quantity")]
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(int id, string name, string description, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
